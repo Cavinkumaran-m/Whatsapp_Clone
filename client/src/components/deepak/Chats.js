58cb927274d1ceade1e59ef7d419cbdab29dce6d
Cavinkumaran M <82271718+Cavinkumaran-m@users.noreply.github.com>
2023-05-07 20:08:38 +0530

    2

--- client/src/components/deepak/Chats.js
+++ client/src/components/deepak/Chats.js
@@ -3,10 +3,19 @@ import style from "./Chats.module.css";
 function Chats(props) {
   return (
     <div className={style.mainDiv}>
-      <img src={props.data.src}></img>
-      <span className={style.name}>{props.data.name}</span>
-      <span className={style.glass}></span>
-      <span className={style.options}></span>
+      <div className={style.image}>
+        <img src={props.data.src}></img>
+      </div>
+      <div className={style.chatDiv}>
+        <div style={{ display: "flex" }}>
+          <span className={style.name}>{props.data.name}</span>
+          <span className={style.last_time_stamp}></span>
+        </div>
+        <div style={{ display: "flex" }}>
+          <span className={style.last_msg}>{props.data.last}</span>
+          <span className={style.notification}></span>
+        </div>
+      </div>
     </div>
   );
 }
